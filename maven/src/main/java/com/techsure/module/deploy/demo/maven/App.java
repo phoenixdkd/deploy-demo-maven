61b4c86a6f46756ee3b210c7af505b6a7edc732e
dengkd <dengkd@techsure.com.cn>
2017-01-16 11:16:41 +0800

    执行时停顿以便检查

--- maven/src/main/java/com/techsure/module/deploy/demo/maven/App.java
+++ maven/src/main/java/com/techsure/module/deploy/demo/maven/App.java
@@ -1,13 +1,20 @@
 package com.techsure.module.deploy.demo.maven;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Press any key to continue..." );
+        new BufferedReader(new InputStreamReader(System.in)).readLine();
+        System.exit(0);
     }
 }
